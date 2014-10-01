5356146ab23a5a03ad9b89211830b7aa4006ccd0
Gustavosdo <gustavo.sdo@gmail.com>
2014-10-01 12:25:06 -0300

    Tratamentos de Caixa aberto para compra e de deleção de registros
    Implementado tratamento para somente permitir o cadastro de uma compra
    caso exista algum caixa com status aberto;
    
    Alterado algumas chaves estrangeiras ultilizadas na tela de compras para
    que as mesmas não deletem os registros em cascata caso ocorra alguma
    deleção proveniente do registro vinculado a dependentes.

--- estagio/compra/models.py
+++ estagio/compra/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from pessoal.models import Fornecedor
 from parametros_financeiros.models import FormaPagamento
 from movimento.models import Produtos
+from django.core.exceptions import ValidationError
 from utilitarios.funcoes_data import date_add_months, date_add_week, date_add_days
 import datetime
 
@@ -18,14 +19,23 @@ class Compra(models.Model):
     data = models.DateTimeField(auto_now_add=True, verbose_name=u'Data da compra')
     desconto = models.DecimalField(max_digits=20, decimal_places=0, blank=True, null=True, verbose_name=u'Desconto (%)', help_text=u'Desconto sob o valor total da compra.')
     status = models.BooleanField(default=False, verbose_name=u'Cancelada?', help_text=u'Marcando o Checkbox, a compra será cancelada e os itens financeiros estornados.')
-    fornecedor = models.ForeignKey(Fornecedor)
-    forma_pagamento = models.ForeignKey(FormaPagamento)
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.PROTECT)
+    forma_pagamento = models.ForeignKey(FormaPagamento, on_delete=models.PROTECT)
     observacao = models.TextField(blank=True, verbose_name=u'observações', help_text="Descreva na área as informações relavantes da compra.")
 
 
     def __unicode__(self):
         return u'%s' % (self.id)
 
+
+    def clean(self):
+        """ 
+        Bloqueia o registro de uma compra quando não há caixa aberto.
+        """
+
+        if not Caixa.objects.filter(status=1).exists() and not self.pk:
+            raise ValidationError('Não há caixa aberto. Para efetivar uma compra é necessário ter o caixa aberto.')
+        
 
     def prazo_primeira_parcela(self, data, num_parcela):
         """
@@ -182,12 +192,16 @@ class ItensCompra(models.Model):
     valor_total = models.DecimalField(max_digits=20, decimal_places=2, verbose_name=u'Total (R$)')
     desconto = models.DecimalField(max_digits=20, decimal_places=0, blank=True, null=True, verbose_name=u'Desconto (%)')
     #status = models.BooleanField(verbose_name=u'Confirma?')
-    produto = models.ForeignKey(Produtos)
+    produto = models.ForeignKey(Produtos, on_delete=models.PROTECT)
     compras = models.ForeignKey(Compra)
 
     class Meta:
         verbose_name = u'Item de Compra'
         verbose_name_plural = "Itens de Compra"
+
+
+    def __unicode__(self):
+        return u'%s' % (self.id)
 
 
     def save(self, *args, **kwargs):
@@ -211,3 +225,4 @@ class ItensCompra(models.Model):
 
 # Importado no final do arquivo para não ocorrer problemas com dependencia circular 
 from contas_pagar.models import ContasPagar, ParcelasContasPagar, Pagamento
+from caixa.models import Caixa

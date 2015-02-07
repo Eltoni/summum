36b176f90e360d42bd646c4d8abed192648ee464
Gustavosdo <gustavo.sdo@gmail.com>
2015-02-07 21:14:19 -0200

    customizado links que apontam para a página inicial do sistema
    Agora todos os links que antes apontavam para "/", agora apontam para
    "/dashboard/".
    Essa alteração fará com que o usuário seja redirecionado sempre para a
    página customizada do sistema.
    
    Somente ao logar na conta é que o usuário visualiza a página inicial
    padrão da administração do django.

--- estagio/static/js/base.js
+++ estagio/static/js/base.js
@@ -1,2 +1,12 @@
 // Anula o navegador de fazer o preenchimento automático dos campos de todo o projeto
 $("input, select, textarea").attr("autocomplete", "off");
+
+
+// defini todos os links apontados para a página inicial do sistema para redirecionar para o dashboard customizado 
+$("a").each(function() { 
+     var href = $(this).attr('href');
+
+     if (href == "/" && !location.href.match("logout")) {   // checa se a url aponta para a página inicial e se a página não é de logout da conta
+         $(this).attr('href', '/dashboard/');               
+     }
+ });

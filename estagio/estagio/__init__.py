39537ee1a5765109f9e1e9248c1c4902643838d9
Gustavosdo <gustavo.sdo@gmail.com>
2015-04-06 08:54:33 -0300

    Detalhe
    Alterado init.py do diretório principal do projeto para que crie o
    diretório necessários para as dependências da biblioteca utilizada caso
    o mesmo não exista.

--- estagio/estagio/__init__.py
+++ estagio/estagio/__init__.py
@@ -1,6 +1,6 @@
 #-*- coding: UTF-8 -*-
-#Cria diretório caso o mesmo não exista
+#Cria diretório das dependências caso o mesmo não exista
 
-# import os
-# if not os.path.exists('estagio/directory'):
-#     os.makedirs('estagio/directory')
+import os
+if not os.path.exists('estagio/static/components'):
+    os.makedirs('estagio/static/components')

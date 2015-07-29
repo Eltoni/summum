d7c1e5dbb35f116208d816e3bd695fd2e264f3d2
Gustavo Santana <gustavo.sdo@gmail.com>
2015-07-28 22:54:25 -0300

    Atualizados templates customizados do projeto.

--- estagio/contas_pagar/views.py
+++ estagio/contas_pagar/views.py
@@ -2,17 +2,21 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import permission_required
-from contas_pagar.models import Pagamento
+from contas_pagar.models import Pagamento, ParcelasContasPagar, ContasPagar
 from django.utils.translation import ugettext_lazy as _
 
 
 def retorna_pagamentos_parcela(request, id_parcela):
     u""" Retorna os pagamentos efetuados para a parcela do contexto. """
     pagamentos = Pagamento.objects.filter(parcelas_contas_pagar=id_parcela).values_list('pk', 'data', 'valor', 'juros', 'multa', 'desconto', 'parcelas_contas_pagar')
+    parcela = ParcelasContasPagar.objects.get(pk=id_parcela)
+
     data = {
         'title': _(u"Pagamentos da parcela"),
-        'app_name': Pagamento._meta.app_label,
-        #'is_popup': True,
+        'app_name': parcela._meta.app_label,
+        'opts': parcela._meta,
+        'has_change_permission': False,
+        'original': parcela,
         'pagamentos': pagamentos,
     }
     return render_to_response('admin/pagamentos_parcela.html', data, context_instance=RequestContext(request))
@@ -21,9 +25,14 @@ def retorna_pagamentos_parcela(request, id_parcela):
 def retorna_pagamentos_conta(request, id_conta):
     u""" Retorna os pagamentos efetuados para a conta do contexto. """
     pagamentos = Pagamento.objects.filter(parcelas_contas_pagar__contas_pagar=id_conta).values_list('pk', 'data', 'valor', 'juros', 'multa', 'desconto', 'parcelas_contas_pagar__contas_pagar')
+    conta = ContasPagar.objects.get(pk=id_conta)
+
     data = {
         'title': _(u"Pagamentos da conta"),
-        'app_name': Pagamento._meta.app_label,
+        'app_name': conta._meta.app_label,
+        'opts': conta._meta,
+        'has_change_permission': False,
+        'original': conta,
         'pagamentos': pagamentos,
     }
     return render_to_response('admin/pagamentos_conta.html', data, context_instance=RequestContext(request))
--- estagio/contas_receber/views.py
+++ estagio/contas_receber/views.py
@@ -2,17 +2,21 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import permission_required
-from contas_receber.models import Recebimento
+from contas_receber.models import Recebimento, ParcelasContasReceber, ContasReceber
 from django.utils.translation import ugettext_lazy as _
 
 
 def retorna_recebimentos_parcela(request, id_parcela):
     u""" Retorna os recebimentos efetuados para a parcela do contexto. """
     recebimentos = Recebimento.objects.filter(parcelas_contas_receber=id_parcela).values_list('pk', 'data', 'valor', 'juros', 'multa', 'desconto', 'parcelas_contas_receber')
+    parcela = ParcelasContasReceber.objects.get(pk=id_parcela)
+
     data = {
         'title': _(u"Recebimentos da parcela"),
-        'app_name': Recebimento._meta.app_label,
-        #'is_popup': True,
+        'app_name': parcela._meta.app_label,
+        'opts': parcela._meta,
+        'has_change_permission': False,
+        'original': parcela,
         'recebimentos': recebimentos,
     }
     return render_to_response('admin/recebimentos_parcela.html', data, context_instance=RequestContext(request))
@@ -21,9 +25,14 @@ def retorna_recebimentos_parcela(request, id_parcela):
 def retorna_recebimentos_conta(request, id_conta):
     u""" Retorna os recebimentos efetuados para a conta do contexto. """
     recebimentos = Recebimento.objects.filter(parcelas_contas_receber__contas_receber=id_conta).values_list('pk', 'data', 'valor', 'juros', 'multa', 'desconto', 'parcelas_contas_receber__contas_receber')
+    conta = ContasReceber.objects.get(pk=id_conta)
+
     data = {
         'title': _(u"Recebimentos da conta"),
-        'app_name': Recebimento._meta.app_label,
+        'app_name': conta._meta.app_label,
+        'opts': conta._meta,
+        'has_change_permission': False,
+        'original': conta,
         'recebimentos': recebimentos,
     }
     return render_to_response('admin/recebimentos_conta.html', data, context_instance=RequestContext(request))
